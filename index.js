14aeed59877256c0d8fdd7968eb4dc9dd000684d
Viegas6354 <156713680+Viegas6354@users.noreply.github.com>
2024-01-16 20:24:00 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Viegas Store","Eu estou online!"];
+const statusMessages = ["Gerenciando Viegas Store","Eu estou online!"];
 
 
 let currentIndex = 0;
